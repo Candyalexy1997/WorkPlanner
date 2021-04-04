d94667bf51a9841cafc9e1fddb841e4cc2ea0d3a
Candice Whiskey <candicewhiskey77@gmail.com>
2021-04-04 00:16:22 -0400

    js work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
-$(document).ready(function () {
+//Current Date at top of page
 
-    var init = false;
-    var now = moment().format('LLLL'); // Day, M/D/Y/T format
+var today = moment();
+$("#currentDay").text(today.format("MMM Do YY"));
+
+    
+
+    // Add Tasks to the local storage
+var addTask = function(){
+
+}
+
+//Store task in the right timeslot
+var getTask = function(){
+
+}
+
+//Create a task
+
+
+
+//Click Handlers to initiate Save Process
+
+// Save Tasks 
+
+
+// Retrieve tasks from localStorage
